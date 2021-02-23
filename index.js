f03cc21066910e56a141acdd336849f44c64ec4a
vrodgreg <75463166+vrodgreg@users.noreply.github.com>
2021-02-23 12:44:32 -0500

    passing all tests

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
 
-  add(item) {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){return a-b});
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos>this.items.length-1) {
+      throw new Error('OutOfBounds');
+    } else {
+    return(this.items[pos])
+    }
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
+    if (this.items.length === 0) {
+    throw new Error('EmptySortedList');
+    } else {
+      this.items.sort(function(a, b){return a-b});
+      return(this.items[this.items.length-1])
+    }
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+      } else {
+        this.items.sort(function(a, b){return a-b});
+        return(this.items[0])
+      }
+  }
+
+  sum() {
+    if (this.items.length === 0) {
+      return(0);
+      } else {
+        return(this.items.reduce((a, b) => a + b, 0));
+      }
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+      } else {
+        return(this.items.reduce((a, b) => a + b, 0) / this.items.length);
+      }
+  }
 }
 
 module.exports = SortedList;
